29e7d889d99a874a14f1b93b1077468ceeaf4d1d
De La Cruz <c.o.dela.cruz@accenture.com>
2021-10-21 15:41:20 +0800

    Working on Advance Function

--- JS Training/Exercise/script.js
+++ JS Training/Exercise/script.js
@@ -1,40 +1,27 @@
-function isUserValid(bool) {
-    return bool;
-}
-const answer = isUserValid(true) ? "You may enter" : "Access denied";
+// closure
 
-console.log(answer);
-
-function moveCommand(direction) {
-    let whatHappen
-
-    switch (direction) {
-        case "forward":
-            whatHappen = "You encouter a bakulaw";
-            break;
-        case "back":
-            whatHappen= "You arrived harong"
-            break;
-        case "right":
-            whatHappen = "You found si naning"
-            break;
-        case "left":
-            whatHappen = "You found yourself"
-            break;
-        default:
-            whatHappen = "Sala ang nilalakawan mo";
+const first = () => {
+    const greet = 'hi';
+    const second = () => {
+        const name = "Dougy";
+        console.log(greet);
     }
 
-    return whatHappen;
+    return second;
 }
 
-console.log(moveCommand('right'));
+const newFunc = first();
+console.log(newFunc());
 
-const name1 = 'Jenny Lee'
-const name2 = 'CJ';
-const greetings1 = `Hello ${name1},  What a beautiful name you have.`;
-const greetings2 = ' Okay CJ.'
+//Currying
+const curriedMultyiply = (a) => (b) => a * b;
+const multiply = curriedMultyiply(5)(8);
 
-const pinagsama = (a, b) => a + b;
+console.log(multiply);
 
-console.log(pinagsama(greetings1, greetings2));
+//Compose
+const compose = (f, g) => (a) => f(g(a));
+const sum = (num) => num + 1;
+const sub = (num) => num - 1;
+
+console.log(compose(sum, sub)(5));
